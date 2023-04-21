e20bf41269f4e589aa637705ab9e8f8af0e4360f
jerson100 <juamkoo@gmail.com>
2023-04-20 19:05:54 -0500

    Add files

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,1 +1,7 @@
-export const schemaTypes = []
+import pin from './pin'
+import postedBy from './postedBy'
+import save from './save'
+import user from './user'
+import comment from './comment'
+
+export const schemaTypes = [pin, user, postedBy, comment, save]
