a0f5494f5f99bcedacae3eb008664ecb461310b0
jerson100 <juamkoo@gmail.com>
2023-05-06 10:24:15 -0500

    space

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -3,6 +3,6 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'ihu6zpbm',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
 })
